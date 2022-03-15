db87702873e336371c199d5cc87ad77ca1865c61
Jay Klehr <jay@diablomedia.com>
2022-03-14 18:38:11 -0600

    fixing config location

--- src/Module.php
+++ src/Module.php
@@ -35,7 +35,7 @@ class Module
 
     public function getConfig(): array
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getServiceConfig(): array
